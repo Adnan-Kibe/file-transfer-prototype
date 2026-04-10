e527c70df66d02922f1ccd540c14128232d0eb42
AdnanK1 <adnang680@gmail.com>
2026-04-10 19:32:55 +0300

    Added three functions

--- documents/root.go
+++ documents/root.go
@@ -1,6 +1,11 @@
 package documents
 
-import "os"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
 
 func ReadingFile(path string) ([]byte, error) {
 	file, err := os.Open(path)
@@ -9,4 +14,32 @@ func ReadingFile(path string) ([]byte, error) {
 	}
 	defer file.Close()
 	return os.ReadFile(path)
+}
+
+func CheckFolderStucture(path string) ([]os.DirEntry, error) {
+	info, err := os.ReadDir(path)
+	if err != nil {
+		if os.IsNotExist(err){
+			fmt.Println("folder does not exist")
+			return nil, err
+		}
+	}
+
+	return info, nil
+}
+
+
+
+func FullWalkDown(path string) error {
+	return filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			fmt.Println(path + " -> directory")
+		} else {
+			fmt.Println("L-> " + path + " -> file")
+		}
+		return nil
+	})
 }
